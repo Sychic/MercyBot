f5a2bc2f70fc41bd3131b3ae652fb831effd9657
Sychic <Sychic@github.com>
2020-08-01 14:26:53 -0400

    message change

--- bot.js
+++ bot.js
@@ -13,9 +13,9 @@ client.on('message', message => {
 });
 
 client.on('guildMemberAdd', member => {
+    let emoji = member.guild.emojis.cache.find(emoji => emoji.id === "739051596616564796");
     let embed = new Discord.MessageEmbed()
-        .setDescription(`Welcome <@${member.user.id}>!`)
-        .addField("­","Weeeelcomeeee ^-^ Be sure to read <#738470770317066356> and enjoy your stay")
+        .setDescription(`Hey <@${member.user.id}>, welcome to Mercy's Heaven! Be sure to read <#738470770317066356> and check <#739035968937394230> if you are intrested in getting some colours ! ${emoji}`)
         .setColor("#F879C2");
     member.guild.channels.cache.find(channel => channel.id === "738491993390448811").send({embed});
 });
